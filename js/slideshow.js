9c096e732d9c32ec5f237857d6da004cec36c43f
Phuc <sucanabo@gmail.com>
2019-06-24 22:05:07 +0700

    first commit

---
+++ js/slideshow.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.sildeshow').slick({
+      dots:true,
+      fade:true,
+      autoplay:true,
+      autoplaySpeed:3000,
+      nextArrow: '<i class="fas fa-chevron-right"></i>',
+      prevArrow: '<i class="fas fa-chevron-left"></i>'
+    });
+  });
