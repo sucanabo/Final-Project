744b97dd3913f278c8ccca4c85a3e876835cf67c
Phuc <sucanabo@gmail.com>
2019-06-26 00:18:49 +0700

    second-commit

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
       autoplay:true,
       autoplaySpeed:3000,
       nextArrow: '<i class="fas fa-chevron-right"></i>',
-      prevArrow: '<i class="fas fa-chevron-left"></i>'
+      prevArrow: '<i class="fas fa-chevron-left"></i>',
+      focusOnSelect:true
     });
   });
