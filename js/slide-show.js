5f857548fb2587c3654f444f5b57bceaca1f9ad0
Phuc <sucanabo@gmail.com>
2019-08-22 22:26:55 +0700

    update-22.8/2

--- js/slide-show.js
+++ js/slide-show.js
@@ -60,3 +60,23 @@ function move() {
       }
     }
   }
+//
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("team-slides");
+  var dots = document.getElementsByClassName("dots-2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+}
